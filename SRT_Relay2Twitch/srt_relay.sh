90cac08fdcfb53ad60e025b38101b5d3bb27aeef
Stéphane Guerreau <contact@arcadia-labs.com>
2022-12-06 15:15:56 +0100

    Update srt_relay.sh

--- SRT_Relay2Twitch/srt_relay.sh
+++ SRT_Relay2Twitch/srt_relay.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+autoreconnect=true
 localip="LOCAL IP"
 port="PORT"
+latency="200000"
 endpoint="ENDPOINT"
 streamkey="STREAMKEY"
 
-ffmpeg \
-        -i "srt://"$localip":"$port"?mode=listener&transtype=live&latency=200000" \
-        -c copy \
-        -f flv \
-        $endpoint$streamkey
+function stream {
+	ffmpeg \
+        	-i "srt://"$localip":"$port"?mode=listener&transtype=live&latency="$latency \
+        	-c copy \
+        	-f flv \
+        	$endpoint$streamkey
+}
+
+if [ "$autoreconnect" = true ] 
+then
+	for (( ; ; ))
+	do
+		stream
+		sleep 1
+	done
+else
+	stream
+fi
